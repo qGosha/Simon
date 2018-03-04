d19bb7989c3412a8d1ef3ca838d57e2fa2d06da1
Igor <qgosha@list.ru>
2018-03-03 19:13:20 -0700

    add

--- src/ColorPanel.js
+++ src/ColorPanel.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Control } from './Control';
 import { Result } from './Result';
 
-const sounds = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
-'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'];
+
 
 export class ColorPanel extends React.Component {
  constructor(props) {
@@ -17,7 +16,9 @@ export class ColorPanel extends React.Component {
     count: 0,
     speed: 1500,
     error: false,
-    restarting: false
+    restarting: false,
+    inRestartProcess: false,
+    win: false
   }
   this.onGameStarted = this.onGameStarted.bind(this);
  }
@@ -38,6 +39,31 @@ export class ColorPanel extends React.Component {
  });
  }
 
+win() {
+this.setState({win: true, error: true, yourTurn: false});
+const prom = () => {
+  return new Promise( (resolve) => {
+    for (let i = 0; i <= 8; i++) {
+      setTimeout( () => {
+        this.switchColors('add', 250)
+            .then( () => {
+              if(i > 7) resolve();
+              this.switchColors('remove', 250) })
+      }, 1000 * i);
+    }
+  });
+}
+
+prom()
+.then( (response) => {
+  console.log(response);
+    this.gameAborted();
+    this.aiTurn();
+}).catch(error => {
+    alert(error);
+  });;
+}
+
 error() {
   this.setState({error: true, yourTurn: false});
   this.switchColors('add', 600)
@@ -53,8 +79,9 @@ error() {
 }
 
 onGameStarted() {
+  if(this.state.inRestartProcess) return;
   if(this.state.count)  {
-    this.setState({error: true, restarting: true});
+    this.setState({error: true, restarting: true, inRestartProcess: true});
     this.switchColors('add', 400)
         .then( () => { return this.switchColors('remove', 400) }).then( () => {
       this.gameAborted();
@@ -71,11 +98,15 @@ success() {
   const yourTurn = !this.state.yourTurn;
   var steps = this.state.steps;
   var speed = this.state.speed;
+  if(steps.length + 1 === 20) {
+    this.win();
+    return;
+  }
   steps.push(Math.floor(Math.random() * 4));
-  if(steps.length > 3 && steps.length <= 7) speed = 1200;
-  if(steps.length > 7 && steps.length <= 12) speed = 1000;
-  if(steps.length > 12 && steps.length <= 15) speed = 750;
-  if(steps.length > 15 && steps.length <= 20) speed = 620;
+    if(steps.length > 3 && steps.length <= 7) speed = 1200;
+    if(steps.length > 7 && steps.length <= 12) speed = 1000;
+    if(steps.length > 12 && steps.length <= 15) speed = 750;
+    if(steps.length > 15 && steps.length <= 20) speed = 620;
   this.setState({steps, currentStep, yourTurn, speed});
   this.aiTurn();
 }
@@ -90,14 +121,11 @@ click(e) {
    return;
  }
 
-  let play = new Audio(sounds[pos]);
-  play.addEventListener('play', () => {
-   document.querySelectorAll('.panel div[data-pos]')[pos].classList.add('bright');
-});
-play.addEventListener('ended', () => {
- document.querySelectorAll('.panel div[data-pos]')[pos].classList.remove('bright');
-});
-play.play();
+let play = document.querySelectorAll('.panel audio')[pos];
+play.load();
+play.play().then(() => setTimeout(() => {
+  play.onended();
+}, play.duration * 1000)).catch((error) => console.log("Error: " +error));
 currentStep = currentStep + 1;
 if (currentStep === steps.length) {
   this.success();
@@ -114,11 +142,25 @@ gameAborted() {
     count: 0,
     steps: [Math.floor(Math.random() * 4)],
     currentStep: 0,
-    yourTurn: false
+    yourTurn: false,
+    win: false
   });
 }
 
+componentDidMount() {
+          const audio = document.querySelectorAll('.panel audio');
+  Array.prototype.map.call(audio, (elem) => {
+      elem.onplay = () => {
+       elem.parentElement.classList.add('bright');
+   };
+     elem.onended = () => {
+    elem.parentElement.classList.remove('bright');
+  };
+    })
+}
+
 aiTurn(afterError) {
+
  var steps = this.state.steps;
  const count = afterError ? this.state.count : this.state.count + 1;
  const speed = this.state.speed;
@@ -130,15 +172,14 @@ aiTurn(afterError) {
           timerArr.map((item) => clearTimeout(item));
           return;
         }
-        let play = new Audio(sounds[steps[i]]);
-        play.addEventListener('play', () => {
-         document.querySelectorAll('.panel div[data-pos]')[steps[i]].classList.add('bright');
-     });
-        play.addEventListener('ended', () => {
-         document.querySelectorAll('.panel div[data-pos]')[steps[i]].classList.remove('bright');
-         if(i === steps.length - 1) resolve();
-     });
-        play.play();
+
+      let play = document.querySelectorAll('.panel audio')[steps[i]];
+      play.load();
+      play.play().then(() => setTimeout(() => { //we need this because some chrome versions don't fire "ended" event
+        if(i === steps.length - 1) resolve();
+        play.onended();
+      }, play.duration * 1000)).catch((error) => console.log("Error: " +error));
+
       }, speed * (i+1));
     timerArr.push(timeout);
     }
@@ -146,18 +187,22 @@ aiTurn(afterError) {
 
 promise.then( () => {
  const yourTurn = !this.state.yourTurn;
-  this.setState({yourTurn, count});
+ const inRestartProcess = false;
+  this.setState({yourTurn, count, inRestartProcess});
 }).catch((error) => console.log("Error: " +error));
 }
 
 render() {
+  const sounds = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
+  'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'];
   const colors = sounds.map( (item,i) => {
-    return <div key={i} className='colors' data-pos={i} onClick={(e) => this.click(e)}></div>
+    return <div key={i} className='colors' data-pos={i} onClick={(e) => this.click(e)}><audio src={sounds[i]}></audio></div>
   })
   return(
     <div className='panel'>
       {colors}
-      {this.state.error ? <Result error={this.state.error} strict={this.state.strict} restarting={this.state.restarting}/> :
+      {this.state.error || this.state.win ? <Result strict={this.state.strict}
+      restarting={this.state.restarting} win={this.state.win}/> :
         <Control
         onModeChange={() => this.setState({strict: !this.state.strict})}
         onGameStarted={this.onGameStarted}
--- src/Result.js
+++ src/Result.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-export const Result = ({error, strict, restarting}) => {
- return (
-   <section className='Result'><span>{restarting ? 'Restart' : (strict ? 'Try again The Game will restart' : 'Try again')}</span></section>
- )
+export const Result = ({strict, restarting, win}) => {
+  let result;
+  if(win) {
+    result = 'You won the Game!!!';
+  } else if (restarting) {
+    result = 'Restart';
+  } else {
+    if(strict) {
+      result = 'Try again The Game will restart';
+    } else {
+      result = 'Try again';
+    }
+  }
+  return (
+    <section className='Result'><span>{result}</span></section>
+  )
 }
